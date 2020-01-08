7af320fc05a97dd61ef54e59f4993d9f8977564a
chad10 <changda0616@gmail.com>
2020-01-08 17:23:05 +0800

    新增 list, list-item 元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list/list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule
